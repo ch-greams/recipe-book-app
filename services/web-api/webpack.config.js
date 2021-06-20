4be56bd06972da685cbafee8b0aaa9ecbbab1454
Andrei Khvalko <andrey.greams@gmail.com>
2021-06-20 17:34:21 +0300

    Refactor linting a bit

--- services/web-api/webpack.config.js
+++ services/web-api/webpack.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require("path");
 const NodemonPlugin = require("nodemon-webpack-plugin");
 const nodeExternals = require("webpack-node-externals");
