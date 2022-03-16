393d5a73023023b3d41c3db469b38f8fb484db6c
Andrei Khvalko <andrey.greams@gmail.com>
2022-03-16 14:07:01 +0100

    Remove API in TS and replace GH workflow

--- services/web-api/src/common/typings/common.d.ts
+++
@@ -1,9 +0,0 @@
-
-export type None = null | undefined;
-export type Some<T> = T;
-export type Option<T> = None | Some<T>;
-
-
-export type Dictionary<TKey extends string | number | symbol, TValue> = {
-    [key in TKey]?: TValue;
-};
