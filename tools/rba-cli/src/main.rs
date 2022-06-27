480436047b66e8fe1a5c99bb232ecd5da4d389f3
Andrei Khvalko <andrey.greams@gmail.com>
2022-06-28 00:22:58 +0200

    Add CI actions for rba-cli and fix path resolution issue for seed_db function

--- tools/rba-cli/src/main.rs
+++ tools/rba-cli/src/main.rs
@@ -1,5 +1,4 @@
-use clap::{Parser, Args, Subcommand};
-use sqlx::PgPool;
+use clap::{Args, Parser, Subcommand};
 
 mod utils;
 
@@ -24,53 +23,24 @@ struct DatabaseOpts {
 
 #[tokio::main]
 async fn main() {
-
     let cli_opts = CliOpts::parse();
 
     match &cli_opts.command {
         Commands::MigrateDatabase(opts) => {
-            let database_url = opts.database_url.as_deref()
+            let database_url = opts
+                .database_url
+                .as_deref()
                 .unwrap_or("postgres://postgres:password@localhost");
 
-            migrate_db(database_url).await;
-        },
+            utils::migrate_db(database_url).await;
+        }
         Commands::SeedDatabase(opts) => {
-            let database_url = opts.database_url.as_deref()
+            let database_url = opts
+                .database_url
+                .as_deref()
                 .unwrap_or("postgres://postgres:password@localhost");
 
-            seed_db(database_url).await;
+            utils::seed_db(database_url).await;
         }
     }
-}
-
-async fn migrate_db(database_url: &str) {
-    println!("migrating database...");
-
-    let db_pool = PgPool::connect_lazy(database_url).unwrap();
-
-    sqlx::migrate!("database/migrations").run(&db_pool.clone()).await.unwrap();
-
-    println!("migrating is complete!");
-}
-
-async fn seed_db(database_url: &str) {
-    println!("seeding database...");
-
-    let db_pool = PgPool::connect_lazy(database_url).unwrap();
-
-    let seed_list = vec![
-        "database/seeds/00_user.sql",
-        "database/seeds/01_product.sql",
-        "database/seeds/02_custom_unit.sql",
-        "database/seeds/02_direction.sql",
-        "database/seeds/02_favorite_product.sql",
-        "database/seeds/02_ingredient.sql",
-        "database/seeds/02_nutrition_fact.sql",
-        "database/seeds/03_direction_part.sql",
-        "database/seeds/03_ingredient_product.sql",
-    ];
-
-    utils::seed_db(db_pool.clone(), seed_list).await;
-
-    println!("seeding is complete!");
 }
--- tools/rba-cli/src/utils.rs
+++ tools/rba-cli/src/utils.rs
@@ -1,13 +1,44 @@
-use sqlx::{Pool, Postgres};
-use std::fs;
+use sqlx::PgPool;
+use std::{env, fs, path::PathBuf};
 
+pub(crate) async fn migrate_db(database_url: &str) {
+    println!("migrating database...");
 
-pub(crate) async fn seed_db(db_pool: Pool<Postgres>, seed_list: Vec<&str>) {
+    let db_pool = PgPool::connect_lazy(database_url).unwrap();
+
+    sqlx::migrate!("database/migrations")
+        .run(&db_pool.clone())
+        .await
+        .unwrap();
+
+    println!("migrating is complete!");
+}
+
+pub(crate) async fn seed_db(database_url: &str) {
+    println!("seeding database...");
+
+    let db_pool = PgPool::connect_lazy(database_url).unwrap();
+
+    let seed_list = vec![
+        "database/seeds/00_user.sql",
+        "database/seeds/01_product.sql",
+        "database/seeds/02_custom_unit.sql",
+        "database/seeds/02_direction.sql",
+        "database/seeds/02_favorite_product.sql",
+        "database/seeds/02_ingredient.sql",
+        "database/seeds/02_nutrition_fact.sql",
+        "database/seeds/03_direction_part.sql",
+        "database/seeds/03_ingredient_product.sql",
+    ];
 
     let mut txn = db_pool.begin().await.unwrap();
 
+    let base_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+
     for seed_path in seed_list.iter() {
-        let sql_text = fs::read_to_string(seed_path).unwrap();
+        let path = base_dir.join(seed_path);
+
+        let sql_text = fs::read_to_string(path).unwrap();
 
         let query = sqlx::query(&sql_text);
 
@@ -21,4 +52,6 @@ pub(crate) async fn seed_db(db_pool: Pool<Postgres>, seed_list: Vec<&str>) {
     }
 
     txn.commit().await.unwrap();
+
+    println!("seeding is complete!");
 }
