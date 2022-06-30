c8c46be4e13713fd61d39b656e48662540e6cc86
Andrei Khvalko <andrey.greams@gmail.com>
2022-07-01 00:47:30 +0200

    RBA-81: New color scheme

--- services/web-app/common/colors.ts
+++ services/web-app/common/colors.ts
@@ -1,7 +1,8 @@
 
 export enum Color {
-    Default = "#00bfa5",
-    DefaultDark = "#008e76",
+    Default = "#43a047",
+    DefaultDark = "#00701a",
+    DefaultLight = "#76d275",
     Alternative = "#ff6e40",
     White = "#fff",
 }
