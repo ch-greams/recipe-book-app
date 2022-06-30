8e14ec6424cb14da246ab6abdd2d79ec0eaa6b4f
Andrei Khvalko <andrey.greams@gmail.com>
2022-06-30 17:14:25 +0200

    Reduce the amount of font weights to 3

--- services/web-app/pages/_document.tsx
+++ services/web-app/pages/_document.tsx
@@ -6,8 +6,7 @@ const Document: React.FC = () => (
     <Html>
         <Head>
             <link
-                // TODO: Reduce amount of font weights to only used ones from variables.scss (RBA-75)
-                href={"https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"}
+                href={"https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap"}
                 rel={"stylesheet"}
             />
         </Head>
