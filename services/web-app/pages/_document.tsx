0814e74ddbc6a5cfc9d2abc135c576f9e1cddd84
Andrei Khvalko <andrey.greams@gmail.com>
2022-06-30 17:18:35 +0200

    RBA-75: Improve fonts (#112)
    * Fix font loading in production build
    
    * Reduce the amount of font sizes to 4
    
    * Reduce the amount of font weights to 3

---
+++ services/web-app/pages/_document.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Head, Html, Main, NextScript } from "next/document";
+
+
+const Document: React.FC = () => (
+    <Html>
+        <Head>
+            <link
+                href={"https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap"}
+                rel={"stylesheet"}
+            />
+        </Head>
+        <body>
+            <Main />
+            <NextScript />
+        </body>
+    </Html>
+);
+
+Document.displayName = "Document";
+
+export default Document;
