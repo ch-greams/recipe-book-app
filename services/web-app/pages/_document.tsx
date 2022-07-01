b0f2dcf5b1fd58e7795e612a41237e9923bbdd27
Andrei Khvalko <andrey.greams@gmail.com>
2022-07-01 19:06:08 +0200

    Fix favicon

--- services/web-app/pages/_document.tsx
+++ services/web-app/pages/_document.tsx
@@ -5,6 +5,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 const Document: React.FC = () => (
     <Html>
         <Head>
+            <link rel={"shortcut icon"} href={"/favicon.ico"} />
             <link
                 href={"https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap"}
                 rel={"stylesheet"}
